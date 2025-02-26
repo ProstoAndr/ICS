e2b7247a818a08cee200762e093a08c0b9a01e65
Herman7883 <aaagrm6@gmail.com>
2025-02-27 00:42:47 +0700

    Парабола v1
    сделал парболу;
    
    прикрутил кубиту;
    
    изменил метод отрисовки;

--- lib/boundary/usecase/charts_usecase.dart
+++ lib/boundary/usecase/charts_usecase.dart
@@ -2,4 +2,6 @@ import 'package:ics/domain/entity/point.dart';
 
 abstract class ChartsUseCase {
   Future<List<List<Point>>> buildTriangle(List<double> data, int countTerm);
+
+  Future<List<List<Point>>> buildParabolic(List<double> data, int countTerm);
 }
