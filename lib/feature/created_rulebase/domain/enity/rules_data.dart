1cdebf538239d72228f3b091dbbe5073ee1850e9
Andrey Petukhov <a.petuhov@virgroup.xyz>
2025-03-26 00:06:09 +0700

    bugfix/Tnorm
    Исправил создание правил

--- lib/feature/created_rulebase/domain/enity/rules_data.dart
+++ lib/feature/created_rulebase/domain/enity/rules_data.dart
@@ -1,13 +1,16 @@
 import 'package:ics/feature/displaying_graphs/domain/entity/chart_data.dart';
+import 'package:ics/feature/displaying_graphs/domain/entity/plenty.dart';
 
 class RulesData {
   final int countTerm;
   final int countPlenty;
   final List<ChartData> allCharts ;
+  final List<Plenty> plenties;
 
   RulesData({
     required this.countTerm,
     required this.countPlenty,
     required this.allCharts,
+    required this.plenties,
   });
 }
